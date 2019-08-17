74286a60ca8c451ebb072d18a662a1a3718fe974
john le <jcle1@uci.edu>
2019-08-16 19:49:46 -0700

    finished 5b

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import loginService from './services/login'
 import blogService from './services/blogs'
 import userService from './services/users'
@@ -15,7 +15,6 @@ function App() {
   const [user, setUser] = useState('')
   const [users, setUsers] = useState([])
   const [blogs, setBlogs] = useState([])
-  //const [resultBlog, setResultBlog] = useState([])
   const [successMessage, setSuccessMessage] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   // this is for whne the user logs after 
@@ -24,15 +23,14 @@ function App() {
   const [newUrl, setUrl] = useState('')
   const [loginVisible, setLoginVisible] = useState(false)
   const [blogVisible, setBlogVisible] = useState(false)
-  //const [blogsUserCanDelete, setBlogsUserCanDelete] = useState([])
   const updateBlogList = async () => {
     const blogs = await blogService.getAll()
     setBlogs(blogs.sort((a, b) => b.likes - a.likes))
   }
   useEffect(() => {
     userService
-    .getAllUsers()
-    .then(users => setUsers(users) )
+      .getAllUsers()
+      .then(users => setUsers(users) )
   })
   useEffect(() => {    
     //console.log('waht')
@@ -92,24 +90,24 @@ function App() {
     }
     
     blogService
-    .makeABlog(newBlog)
-    .then(response => {
-      setSuccessMessage(`a new blog ${newTitle} by ${newAuthor} added`)
-      setAuthor('')
-      setTitle('')
-      setUrl('')
-      updateBlogList()
-    })
+      .makeABlog(newBlog)
+      .then(response => {
+        setSuccessMessage(`a new blog ${newTitle} by ${newAuthor} added`)
+        setAuthor('')
+        setTitle('')
+        setUrl('')
+        updateBlogList()
+      })
   }
   
   const handleLikes = (event) => {
     event.preventDefault()
-    event.cancelBubble = true; // alias to Event.stopPropogation
+    event.cancelBubble = true // alias to Event.stopPropogation
     const idToCheck = event.target.value //stops bubbling
-    if(event.stopPropagation) event.stopPropagation();
+    if(event.stopPropagation) event.stopPropagation()
     const blogToUpdate = blogs.reduce(function(accumulator, currentValue) {
       if (currentValue.id === idToCheck)
-      {
+      {" 
         accumulator = currentValue
         return accumulator
       }
@@ -129,15 +127,15 @@ function App() {
     console.log(JSON.stringify(newBlog))
     console.log(idToCheck)
     blogService
-    .addALike(idToCheck, newBlog)
-    .then(response => {
-      setSuccessMessage(`a blog ${blogToUpdate.title} has recieved a like`)
-      updateBlogList()
-    })
+      .addALike(idToCheck, newBlog)
+      .then(response => {
+        setSuccessMessage(`a blog ${blogToUpdate.title} has recieved a like`)
+        updateBlogList()
+      })
   }
   const handleDelete = (event) => {
     event.preventDefault()
-    event.cancelBubble = true; // alias to Event.stopPropogation
+    event.cancelBubble = true // alias to Event.stopPropogation
     if(event.stopPropagation) event.stopPropagation();
     //console.log(event.target.value)
     //console.log(event.target.value)
@@ -153,43 +151,23 @@ function App() {
     const confirm = window.confirm(`remove blog ${resultBlog.title} by ${resultBlog.author}`)
     if (confirm) {
       blogService
-      .deleteABlog(event.target.value)
-      .then(response => {
-        setSuccessMessage(`the blog ${resultBlog.title} by ${resultBlog.author} has been deleted  `)
-        updateBlogList()
-      })
+        .deleteABlog(event.target.value)
+        .then(response => {
+          setSuccessMessage(`the blog ${resultBlog.title} by ${resultBlog.author} has been deleted  `)
+          updateBlogList()
+        })
     }
   }
-  
+ 
   const resultForm = () => {
-    const hideWhenVisible = {display: blogVisible ? 'none' : ''}
-    const showWhenVisible = {display: blogVisible ? '' : 'none'}
-    //console.log(user.name)
-    //console.log(JSON.stringify(users))
-    //console.log("my arry " + JSON.stringify(blogs))
-    /*
-    const filterBlogs = () => users.filter(myUser => myUser.name === user.name).map( resultUser => resultUser.blogs.map(blog => {
-      //const newBlog = blogs.filter(myBlog => myBlog.id === blog.id)
-      const newBlog = blogs.reduce(function(accumulator, currentValue) {
-        if (currentValue.id === blog.id) {
-          return currentValue
-        }
-        else {
-          return accumulator
-        }
-      })
-      return (
-        <div>
-          <Blog blog = {newBlog} />
-        </div>
-      )
-    }))
-    */
+    const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+    const showWhenVisible = { display: blogVisible ? '' : 'none' }
+    
     const allBlogs= () => blogs.map(newBlog => {
       //console.log(user)
       //console.log(blogsUserCanDelete)
       return (
-        <div>
+        <div key= {newBlog.id}>
           <Blog blog = {newBlog} username = {user.username} likeHandler = {handleLikes} deleteHandler = {handleDelete}/>
         </div>
       )
@@ -207,13 +185,13 @@ function App() {
         <div style = {showWhenVisible}>
           <h2>create new</h2>
           <ResultForm
-          handleSubmit = {handleSubmitBlog}
-          titleChange = {({target}) => setTitle(target.value) }
-          authorChange = {({target}) => setAuthor(target.value)}
-          urlChange = {({target}) => setUrl(target.value)}
-          title = {newTitle}
-          author = {newAuthor}
-          url = {newUrl}
+            handleSubmit = {handleSubmitBlog}
+            titleChange = {({ target }) => setTitle(target.value) }
+            authorChange = {({ target }) => setAuthor(target.value)}
+            urlChange = {({ target }) => setUrl(target.value)}
+            title = {newTitle}
+            author = {newAuthor}
+            url = {newUrl}
           />
           <button onClick = {() => {setBlogVisible(!blogVisible)}}>cancel</button>
 
@@ -231,32 +209,9 @@ function App() {
     )
   }
 
-  /*
-  <h2>current blogs for {user.name}</h2>
-        <div>
-          {filterBlogs()}
-        </div>
-  */
-  /*
-  <form onSubmit = {() => handleSubmitBlog}>
-            <div>
-            title:
-            <input type= 'text' value = {newTitle} name= 'Title' onChange= {({target}) => setTitle(target.value) }></input>
-            </div>
-            <div>
-            author:
-            <input type= 'text' value = {newAuthor} name = 'Author' onChange = {({target}) => setAuthor(target.value)}></input>
-            </div>
-            <div>
-            url:
-            <input type= 'text' value = {newUrl} name = 'Url' onChange = {({target}) => setUrl(target.value)}></input>
-            </div>
-            <button type= 'submit'>create</button>
-          </form>
-  */
   const loginForm = () => {
-    const hideWhenVisible = {display : loginVisible ? 'none' : ''}
-    const showWhenVisible = {display : loginVisible ? '' : 'none'}
+    const hideWhenVisible = { display : loginVisible ? 'none' : '' }
+    const showWhenVisible = { display : loginVisible ? '' : 'none' }
     return (
       <div>
         <div style = {hideWhenVisible}>
@@ -265,11 +220,11 @@ function App() {
         <ErrorNotification message = {errorMessage}/>
         <div style = {showWhenVisible}>
           <LoginForm 
-          handleSubmit = {handleLogin} 
-          handleUsernameChange = {({target}) => setUsername(target.value)}
-          handlePasswordChange = {({target}) => setPassword(target.value)}
-          username = {username}
-          password = {password}
+            handleSubmit = {handleLogin} 
+            handleUsernameChange = {({ target }) => setUsername(target.value)}
+            handlePasswordChange = {({ target }) => setPassword(target.value)}
+            username = {username}
+            password = {password}
           />
           <button onClick= {() => setLoginVisible(!loginVisible)}>cancel login</button>
         </div>
@@ -290,9 +245,9 @@ function App() {
       </div>
   
       
-    );  
+    )  
   }
   
 }
 
-export default App;
+export default App
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react'
-/*
-const Blog = ({blog, blogs}) => {
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+const Blog = ({ blog, username, likeHandler, deleteHandler }) => {
   
   const [extraInfo, setExtraInfo] = useState(true)
-  const showExtraVisible = {display : extraInfo ? 'none' : ''}
+  //const [extraButton, setExtraButton] = useState(true)
+  //const [blogsUserCanDelete, setBlogsCanDelete] = useState([])
+  const showExtraVisible = { display : extraInfo ? 'none' : '' }
+  
   //const hideExtraVisible = {display : extraInfo ? '' : 'none'}
   const blogStyle = {
     paddingTop: 10,
@@ -12,86 +15,46 @@ const Blog = ({blog, blogs}) => {
     borderWidth: 1,
     marginBottom: 5
   }
-  //console.log(blog)
-  const newBlog = blogs.filter(myBlog => myBlog.id === blog.id)[0]
-  const newBlogUser = newBlog.user
+  const newBlogUser = blog.user
+  //console.log(username)
+  //console.log(username)
+  const showExtraButton = { display : newBlogUser.username === username ? '' : 'none' } // if true show, if not show nothing
   //console.log(newBlog)
-    return (
+  
+  return (
     <div style= {blogStyle}>
       <div onClick ={() => setExtraInfo(!extraInfo)}>
-        <li key= {blog.id}>{blog.title + " " + blog.author + " " + blog.id}</li>
+        <li key= {blog.id}>{blog.title + ' ' + blog.author + ' ' + blog.id}</li>
         <div style = {showExtraVisible}>
-          {newBlog.url}
+          {blog.url}
           <br></br>
-          {`${newBlog.likes} likes`}
+          {`${blog.likes} likes`} <button value = {blog.id} onClick = {likeHandler}>like</button>
           <br></br>
           {`added by ${newBlogUser.username} `}
           <br></br>
-          {JSON.stringify(newBlog)}
+          <div style = {showExtraButton}>
+            <button value = {blog.id} onClick = {deleteHandler}>remove</button>
+          </div>
+          
+            
+            
         </div>
       </div>
     </div>
     
         
-    )
-}
-*/
-const Blog = ({blog, username, likeHandler, deleteHandler}) => {
-  
-  const [extraInfo, setExtraInfo] = useState(true)
-  //const [extraButton, setExtraButton] = useState(true)
-  //const [blogsUserCanDelete, setBlogsCanDelete] = useState([])
-  const showExtraVisible = {display : extraInfo ? 'none' : ''}
-  
-  //const hideExtraVisible = {display : extraInfo ? '' : 'none'}
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-  //console.log(blog)
-  //const newBlog = new Promise(function(resolve,reject) {resolve(blogs.filter(myBlog => myBlog.id === blog.id)[0])})
-  //const newBlog = blogs.filter(myBlog => myBlog.id === blog.id)[0]
-  //console.log(newBlog)
-  //const newBlogUser = newBlog.user
-  //console.log(blog.user)
-  //console.log(blogsUserCanDel)
-  const newBlogUser = blog.user
-  //console.log(username)
-  //console.log(username)
-  const showExtraButton = {display : newBlogUser.username === username ? '' : 'none' } // if true show, if not show nothing
-  //console.log(newBlog)
-  
-    return (
-      <div style= {blogStyle}>
-        <div onClick ={() => setExtraInfo(!extraInfo)}>
-          <li key= {blog.id}>{blog.title + " " + blog.author + " " + blog.id}</li>
-          <div style = {showExtraVisible}>
-            {blog.url}
-            <br></br>
-            {`${blog.likes} likes`} <button value = {blog.id} onClick = {likeHandler}>like</button>
-            <br></br>
-            {`added by ${newBlogUser.username} `}
-            <br></br>
-            <div style = {showExtraButton}>
-              <button value = {blog.id} onClick = {deleteHandler}>remove</button>
-            </div>
-          
-            
-            
-          </div>
-        </div>
-      </div>
-      
-          
-    )
+  )
 
     
 }
 
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  username: PropTypes.string.isRequired,
+  likeHandler: PropTypes.func.isRequired,
+  deleteHandler: PropTypes.func.isRequired
 
+}
 export default Blog
 
 //. for button we can use value ={value we want to pass}
